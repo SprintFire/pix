9e7f2ab95fa1e76870518cf777f9749f7b4654b2
danielpaul <danielpaul2008@gmail.com>
2016-02-05 20:47:23 +0000

    created product model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205203811) do
+ActiveRecord::Schema.define(version: 20160205204533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.text     "description"
+    t.decimal  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string   "name"
