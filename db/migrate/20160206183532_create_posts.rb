8e0931f16665c0000b6cfa235ef8d3007b3832af
danielpaul <danielpaul2008@gmail.com>
2016-02-06 18:38:22 +0000

    updated description type

--- db/migrate/20160206183532_create_posts.rb
+++ db/migrate/20160206183532_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :name
-      t.string :description
+      t.text :description
 
       t.timestamps null: false
     end
