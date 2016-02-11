41a73abf6b55c02ad854bcf9b0a4cb74eca64be7
danielpaul <danielpaul2008@gmail.com>
2016-02-11 17:11:37 +0000

    Added devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  has_may :posts
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  has_many :posts
 end
